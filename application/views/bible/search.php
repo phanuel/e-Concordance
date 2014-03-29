f96e8ef7f3b2a664383c6712dd49cbb687be690d
phanuel <phanu@stillnorth.net>
2014-03-29 23:07:54 +0100

    search: Display search advices on start page and also if search gives no result. Fixes #4, fixes #11

--- application/views/bible/search.php
+++ application/views/bible/search.php
@@ -24,17 +24,17 @@
 <?php if (isset($exception)): ?>
     <div class="row">
         <div class="span12">
-            <div class="alert">
-                <strong>Erreur:</strong> <?php echo $exception->getMessage(); ?>
+            <div class="alert alert-error">
+                <span class="label label-important">Erreur</span> <?php echo $exception->getMessage(); ?>
             </div>
         </div>
     </div>
 <?php else: ?>
-    <?php if ($mode == "verb"): ?>
+    <?php if ($mode == "verb" && $count > 0): ?>
         <div class="row">
             <div class="span12">
                 <div class="alert alert-info">
-                    <strong>Attention:</strong> Ce mode de recherche peut retourner des résultats inattendus. Par exemple, une recherche sur le verbe "taire" englobera sans distinction les versets contenant le passé composé "tu" et le pronom "tu" qui sont homographes.
+                    <span class="label label-info">Attention</span> Des résultats inattendus peuvent être retournés. Par exemple, une recherche sur le verbe "pleuvoir" englobera sans distinction les versets contenant le participe passé "plu" du verbe <b>plaire</b>, qui est homographe avec celui du verbe pleuvoir.
                 </div>
             </div>
         </div>
@@ -42,17 +42,18 @@
 
     <?php
     if ($count != null) {
-        $n = $count;
-
-        if ($n == 0) {
+        $prefix = $suffix = "";
+        
+        if ($count == 0) {
             $prefix = "Pas de résultat";
-        } else if ($n == 1) {
+            $suffix = " avec ce mode de recherche";
+        } else if ($count == 1) {
             $prefix = "1 occurence";
         } else {
-            $prefix = $n . " occurences";
+            $prefix = $count . " occurences";
         }
 
-        $resultText = $prefix . " pour <i>" . $query . "</i>";
+        $resultText = $prefix . " pour <i>" . $query . "</i>" . $suffix;
 
         if ($mode == "verb") {
             $resultText .= " <small>(formes conjuguées du verbe)</small>";
@@ -69,47 +70,104 @@
     <?php if ($count != null): ?>
         <div class="row">
             <div class="span12">
-                <div class="searchResults thumbnail">
+                <div class="searchResults<?php echo ($count == 0) ? " alert" : " thumbnail"; ?>">
                     <div class="searchResultsText">
                         <h2 title="Résultats de la recherche"><?php echo $resultText ?></h2>
-                        <table>
-                            <?php foreach ($results as $result): ?>
-                                <?php
-                                $url = base_url() . "bible/read/" . $result->book_identifier . "/" . $result->chapter . "/" . $result->verse;
-                                $link = "<a href='" . $url . "'>" . $result->book_abbreviation . " " . $result->chapter . " v." . $result->verse . "</a>";
-                                ?>
-                                <tr>
-                                    <td class="searchVerseNumber">
-                                        <?php echo $link; ?>
-                                    </td>
-                                    <td>
-                                        <?php
-                                            $string = $result->text;
-                                            $highlighted = '<span style="background-color:#D9EDF7;">$0</span>';
+                        <?php if ($count > 0): ?>
+                            <table>
+                                <?php foreach ($results as $result): ?>
+                                    <?php
+                                    $url = base_url() . "bible/read/" . $result->book_identifier . "/" . $result->chapter . "/" . $result->verse;
+                                    $link = "<a href='" . $url . "'>" . $result->book_abbreviation . " " . $result->chapter . " v." . $result->verse . "</a>";
+                                    ?>
+                                    <tr>
+                                        <td class="searchVerseNumber">
+                                            <?php echo $link; ?>
+                                        </td>
+                                        <td>
+                                            <?php
+                                                $string = $result->text;
+                                                $highlighted = '<span style="background-color:#D9EDF7;">$0</span>';
 
-                                            // highlighting words corresponding to the query entered by the user
-                                            if (isset($flections)) {
-                                                foreach($flections->result() as $flection) {
-                                                    $array[] = $flection->stem.$flection->ending;
-                                                }
-                                                
-                                                $string = preg_replace('~('.implode('|', $array).'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
-                                            }else {
-                                                if ($case_sensitive == true) {
-                                                    $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~s', $highlighted, $string);
+                                                // highlighting words corresponding to the query entered by the user
+                                                if (isset($flections)) {
+                                                    foreach($flections->result() as $flection) {
+                                                        $array[] = $flection->stem.$flection->ending;
+                                                    }
+
+                                                    $string = preg_replace('~('.implode('|', $array).'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
                                                 }else {
-                                                    $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
+                                                    if ($case_sensitive == true) {
+                                                        $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~s', $highlighted, $string);
+                                                    }else {
+                                                        $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
+                                                    }
                                                 }
-                                            }
-                                        ?>
-                                        
-                                        <?php echo $string; ?>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </table>
+                                            ?>
+
+                                            <?php echo $string; ?>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </table>
+                        <?php else: ?>
+                            <p>
+                                <span class="label label-warning">Important</span>
+                                Vérifiez que vos termes de recherche sont correctement orthografiés et lisez attentivement les instructions ci-dessous afin d'éviter toute mauvaise manipulation.
+                            </p>
+                        <?php endif; ?>
                     </div>
                 </div>
+            </div>
+        </div>
+    <?php endif; ?>
+    <?php if ($count == null || $count == 0): ?>
+        <div class="row">
+            <div class="span8 offset2 searchHelp">
+                <h2>Modes de recherche</h2>
+                <ul>
+                    <li>
+                        <h3>par mots (ordre indifférent)</h3>
+                        <p>À utiliser dans la plupart des cas.</p>
+                        <p>
+                            <span class="label label-info">Exemple</span>
+                            Pour trouver ce verset d'Ésaïe qui parle d'une sentinelle dans la nuit, faites une recherche avec pour mots-clés: "<a href="<?php echo base_url(); ?>bible/search?query=sentinelle+nuit&mode=and">sentinelle nuit</a>".
+                        </p>
+                        <p>
+                            <span class="label label-important">Attention</span>
+                            Évitez de rechercher des portions de phrases telles que: "Sentinelle où en est la nuit ?", qui ne retourneront aucun résultat, car le verset en question contient en réalité: "Sentinelle, <b>à quoi</b> en est la nuit ?".
+                        </p>
+                        <p>La ponctuation et l'ordre des mots ne sont pas pris en compte dans ce mode de recherche.</p>
+                    </li>
+                    <li>
+                        <h3>chaîne de caractères</h3>
+                        <p>Se borne strictement à l'enchaînement des caractères recherchés. Utile pour la recherche de mots basés sur le même radical.</p>
+                        <p>
+                            <span class="label label-info">Exemple</span>
+                            Pour trouver tous les mots basés sur le radical "continuel" (<b>continuel</b>s, <b>continuel</b>le, <b>continuel</b>lement, etc.): rechercher "<a href="<?php echo base_url(); ?>bible/search?query=continuel&mode=literal">continuel</a>" avec ce mode de recherche.
+                        </p>
+                        <p>
+                            <span class="label label-important">Attention</span>
+                            Des résultats inattendus peuvent être retournés. Par exemple, une recherche sur le morphème "<a href="<?php echo base_url(); ?>bible/search?query=stab&mode=literal">stab</a>" (<b>stab</b>le, <b>stab</b>ilité, etc.) retournera aussi "inconte<b>stab</b>le".
+                        </p>
+                        <p>La ponctuation est prise en compte dans la chaîne de caractères.</p>
+                    </li>
+                    <li>
+                        <h3>formes conjuguées du verbe</h3>
+                        <p>À partir de l'<b>infinitif</b> d'un verbe, trouve les versets contenant les différentes formes conjuguées.</p>
+                        <p>
+                            <span class="label label-info">Exemple</span>
+                            Utilisez ce mode de recherche pour trouver toutes les occurences du verbe "<a href="<?php echo base_url(); ?>bible/search?query=jaillir&mode=verb">jaillir</a>".
+                        </p>
+                        <p>
+                            <span class="label label-important">Attention</span>
+                            Des résultats inattendus peuvent être retournés. Par exemple, une recherche sur le verbe "<a href="<?php echo base_url(); ?>bible/search?query=pleuvoir&mode=verb">pleuvoir</a>" englobera sans distinction les versets contenant le participe passé "plu" du verbe <b>plaire</b>, qui est homographe avec celui du verbe pleuvoir.
+                        </p>
+                        <p>Ce mode de recherche est considérablement plus lent car il nécessite une requête prenant en compte toutes les déclinaisons possibles du verbe en question, soit plus d'une cinquantaine dans la plupart des cas.</p>
+                    </li>
+                </ul>
+                <h2>Il y a un problème ?</h2>
+                <p>N'hésitez pas à nous le signaler via notre <a href='<?php echo base_url(); ?>index/contact'>formulaire de contact</a>.</p>
             </div>
         </div>
     <?php endif; ?>
--- application/views/songs/search.php
+++ application/views/songs/search.php
@@ -3,8 +3,8 @@
 <?php if (isset($hymnBookNotFoundException)): ?>
     <div class="row">
         <div class="span12">
-            <div class="alert">
-                <strong>Erreur:</strong> <?php echo $hymnBookNotFoundException->getMessage(); ?>
+            <div class="alert alert-error">
+                <span class="label label-important">Erreur</span> <?php echo $hymnBookNotFoundException->getMessage(); ?>
             </div>
         </div>
     </div>
@@ -32,17 +32,17 @@
     <?php if (isset($exception)): ?>
         <div class="row">
             <div class="span12">
-                <div class="alert">
-                    <strong>Erreur:</strong> <?php echo $exception->getMessage(); ?>
+                <div class="alert alert-error">
+                    <span class="label label-important">Erreur</span> <?php echo $exception->getMessage(); ?>
                 </div>
             </div>
         </div>
     <?php else: ?>
-        <?php if ($mode == "verb"): ?>
+        <?php if ($mode == "verb" && $count > 0): ?>
             <div class="row">
                 <div class="span12">
                     <div class="alert alert-info">
-                        <strong>Attention:</strong> Ce mode de recherche peut retourner des résultats inattendus. Par exemple, une recherche sur le verbe "taire" englobera sans distinction les versets contenant le passé composé "tu" et le pronom "tu" qui sont homographes.
+                        <span class="label label-info">Attention</span> Des résultats inattendus peuvent être retournés. Par exemple, une recherche sur le verbe "pleuvoir" englobera sans distinction les versets contenant le participe passé "plu" du verbe <b>plaire</b>, qui est homographe avec celui du verbe pleuvoir.
                     </div>
                 </div>
             </div>
@@ -50,17 +50,18 @@
 
         <?php
         if ($count != null) {
-            $n = $count;
-
-            if ($n == 0) {
+            $prefix = $suffix = "";
+            
+            if ($count == 0) {
                 $prefix = "Pas de résultat";
-            } else if ($n == 1) {
+                $suffix = " avec ce mode de recherche";
+            } else if ($count == 1) {
                 $prefix = "1 occurence";
             } else {
-                $prefix = $n . " occurences";
+                $prefix = $count . " occurences";
             }
 
-            $resultText = $prefix . " pour <i>" . $query . "</i>";
+            $resultText = $prefix . " pour <i>" . $query . "</i>" . $suffix;
 
             if ($mode == "verb") {
                 $resultText .= " <small>(formes conjuguées du verbe)</small>";
@@ -77,55 +78,112 @@
         <?php if ($count != null): ?>
             <div class="row">
                 <div class="span12">
-                    <div class="searchResults thumbnail">
+                    <div class="searchResults<?php echo ($count == 0) ? " alert" : " thumbnail"; ?>">
                         <div class="searchResultsText">
                             <h2 title="Résultats de la recherche"><?php echo $resultText ?></h2>
-                            <table>
-                                <?php foreach ($results as $result): ?>
-                                    <?php
-                                    $url = base_url() . "songs/read/" . $hymn_book_identifier . "/" . $result->song_number . "/" . $result->verse;
+                            <?php if ($count > 0): ?>
+                                <table>
+                                    <?php foreach ($results as $result): ?>
+                                        <?php
+                                        $url = base_url() . "songs/read/" . $hymn_book_identifier . "/" . $result->song_number . "/" . $result->verse;
 
-                                    if ($result->verse == 0) {
-                                        $verse = "refrain";
-                                    }else {
-                                        $verse = "verset ".$result->verse;
-                                    }
+                                        if ($result->verse == 0) {
+                                            $verse = "refrain";
+                                        }else {
+                                            $verse = "verset ".$result->verse;
+                                        }
 
-                                    $link = "<a href='" . $url . "'>" . $result->song_number ."</a>";
-                                    ?>
-                                    <tr>
-                                        <td class="searchSongNumber">
-                                            <h1><?php echo $link; ?></h1>
-                                            <div style="font-size:0.9em;"><?php echo $verse; ?></div>
-                                        </td>
-                                        <td class="songVerseText">
-                                            <?php
-                                                $string = $result->text;
-                                                $highlighted = '<span style="background-color:#D9EDF7;">$0</span>';
+                                        $link = "<a href='" . $url . "'>" . $result->song_number ."</a>";
+                                        ?>
+                                        <tr>
+                                            <td class="searchSongNumber">
+                                                <h1><?php echo $link; ?></h1>
+                                                <div style="font-size:0.9em;"><?php echo $verse; ?></div>
+                                            </td>
+                                            <td class="songVerseText">
+                                                <?php
+                                                    $string = $result->text;
+                                                    $highlighted = '<span style="background-color:#D9EDF7;">$0</span>';
 
-                                                // highlighting words corresponding to the query entered by the user
-                                                if (isset($flections)) {
-                                                    foreach($flections->result() as $flection) {
-                                                        $array[] = $flection->stem.$flection->ending;
-                                                    }
+                                                    // highlighting words corresponding to the query entered by the user
+                                                    if (isset($flections)) {
+                                                        foreach($flections->result() as $flection) {
+                                                            $array[] = $flection->stem.$flection->ending;
+                                                        }
 
-                                                    $string = preg_replace('~('.implode('|', $array).'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
-                                                }else {
-                                                    if ($case_sensitive == true) {
-                                                        $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~s', $highlighted, $string);
+                                                        $string = preg_replace('~('.implode('|', $array).'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
                                                     }else {
-                                                        $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
+                                                        if ($case_sensitive == true) {
+                                                            $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~s', $highlighted, $string);
+                                                        }else {
+                                                            $string = preg_replace('~('.$query.'[a-zA-Z]{0,45})(?![^<]*[>])~is', $highlighted, $string);
+                                                        }
                                                     }
-                                                }
-                                            ?>
+                                                ?>
 
-                                            <?php echo nl2br($string); ?><br />
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </table>
+                                                <?php echo nl2br($string); ?><br />
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </table>
+                            <?php else: ?>
+                                <p>
+                                    <span class="label label-warning">Important</span>
+                                    Vérifiez que vos termes de recherche sont correctement orthografiés et lisez attentivement les instructions ci-dessous afin d'éviter toute mauvaise manipulation.
+                                </p>
+                        <?php endif; ?>
                         </div>
                     </div>
+                </div>
+            </div>
+        <?php endif; ?>
+        <?php if ($count == null || $count == 0): ?>
+            <div class="row">
+                <div class="span8 offset2 searchHelp">
+                    <h2>Modes de recherche</h2>
+                    <ul>
+                        <li>
+                            <h3>par mots (ordre indifférent)</h3>
+                            <p>À utiliser dans la plupart des cas.</p>
+                            <p>
+                                <span class="label label-info">Exemple</span>
+                                Pour trouver ce verset qui parle d'une profonde et pure source où puise la foi, faites une recherche avec pour mots-clés: "<a href="<?php echo base_url(); ?>songs/search/<?php echo $hymn_book_identifier; ?>?query=profonde+source&mode=and">profonde source</a>" ou "<a href="<?php echo base_url(); ?>songs/search/<?php echo $hymn_book_identifier; ?>?query=pure+source&mode=and">pure source</a>".
+                            </p>
+                            <p>
+                                <span class="label label-important">Attention</span>
+                                Évitez de rechercher des vers entiers tels que: "profonde et pure source où puise la foi", qui ne retourneront aucun résultat, car le vers en question est en réalité: "Profonde et pure source où <b>vient puiser</b> la foi".
+                            </p>
+                            <p>La ponctuation et l'ordre des mots ne sont pas pris en compte dans ce mode de recherche.</p>
+                        </li>
+                        <li>
+                            <h3>chaîne de caractères</h3>
+                            <p>Se borne strictement à l'enchaînement des caractères recherchés. Utile pour la recherche de mots basés sur le même radical.</p>
+                            <p>
+                                <span class="label label-info">Exemple</span>
+                                Pour trouver tous les mots basés sur le morphème "faibl" (<b>faibl</b>e, <b>faibl</b>esse, af<b>faibl</b>i, etc.): rechercher "<a href="<?php echo base_url(); ?>songs/search/<?php echo $hymn_book_identifier; ?>?query=faibl&mode=literal">faibl</a>" avec ce mode de recherche.
+                            </p>
+                            <p>
+                                <span class="label label-important">Attention</span>
+                                Des résultats inattendus peuvent être retournés. Par exemple, une recherche sur le morphème "<a href="<?php echo base_url(); ?>songs/search/<?php echo $hymn_book_identifier; ?>?query=tendr&mode=literal">tendr</a>" (<b>tendr</b>e, <b>tendr</b>es, <b>tendr</b>esse, etc.) retournera aussi "at<b>tendr</b>e" et "en<b>tendr</b>e".
+                            </p>
+                            <p>La ponctuation est prise en compte dans la chaîne de caractères.</p>
+                        </li>
+                        <li>
+                            <h3>formes conjuguées du verbe</h3>
+                            <p>À partir de l'<b>infinitif</b> d'un verbe, trouve les versets contenant les différentes formes conjuguées.</p>
+                            <p>
+                                <span class="label label-info">Exemple</span>
+                                Utilisez ce mode de recherche pour trouver toutes les occurences du verbe "<a href="<?php echo base_url(); ?>songs/search/<?php echo $hymn_book_identifier; ?>?query=jaillir&mode=verb">jaillir</a>".
+                            </p>
+                            <p>
+                            <span class="label label-important">Attention</span>
+                            Des résultats inattendus peuvent être retournés. Par exemple, une recherche sur le verbe "<a href="<?php echo base_url(); ?>songs/search/<?php echo $hymn_book_identifier; ?>?query=pleuvoir&mode=verb">pleuvoir</a>" englobera sans distinction les versets contenant le participe passé "plu" du verbe <b>plaire</b>, qui est homographe avec celui du verbe pleuvoir.
+                        </p>
+                        <p>Ce mode de recherche est considérablement plus lent car il nécessite une requête prenant en compte toutes les déclinaisons possibles du verbe en question, soit plus d'une cinquantaine dans la plupart des cas.</p>
+                        </li>
+                    </ul>
+                    <h2>Il y a un problème ?</h2>
+                    <p>N'hésitez pas à nous le signaler via notre <a href='<?php echo base_url(); ?>index/contact'>formulaire de contact</a>.</p>
                 </div>
             </div>
         <?php endif; ?>
