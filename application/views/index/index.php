77f2bb1d0d0b435a432115cad724343128d2d1e8
phanuel <phanu@stillnorth.net>
2014-03-30 12:12:09 +0200

    Improve homepage layout for mobiles. Fixes #10

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -24,67 +24,48 @@
     </div>
 </div>
 
-<div class="row">
-    <div class="main_categories">
-        <table width="100%">
-            <tr>
-                <td>
-                    <br />
-                    <p>
-                        La Bible, version J.N. Darby.
-                    </p>
-                    <p>
-                        <a href="<?php echo base_url(); ?>bible/search" class="bold" title="Rechercher dans la Bible J.N. Darby">Moteur de recherche</a><br />
-                        <a href="<?php echo base_url(); ?>bible" class="bold" title="Lire la Bible">Lecture</a><br />
-                        <br />
-                    </p>
-                </td>
-                <td width="200px">
-                    <a class="main_categories_logo bible_logo" href="<?php echo base_url(); ?>bible"></a>
-                </td>
-            </tr>
-        </table>
-        <hr />
+<div class="row main-category">
+    <div class="span4 offset2">
+        <a class="main_categories_logo bible_logo" href="<?php echo base_url(); ?>bible"></a>
     </div>
-    
-    <div class="main_categories">
-        <table width="100%">
-            <tr>
-                <td>
-                    <br />
-                    <p>
-                        Recueil de cantiques.
-                    </p>
-                    <p>
-                        <a href="<?php echo base_url(); ?>songs/search/hymnes-et-cantiques" class="bold" title="Rechercher dans le recueil Hymnes et Cantiques">Moteur de recherche</a><br />
-                        <a href="<?php echo base_url(); ?>songs/index/hymnes-et-cantiques" class="bold" title="Index du recueil Hymnes et Cantiques">Index</a><br />
-                    </p>
-                </td>
-                <td width="200px">
-                    <a class="main_categories_logo hetc_logo" href="<?php echo base_url(); ?>songs/readall/hymnes-et-cantiques"></a>
-                </td>
-            </tr>
-        </table>
-        <hr />
+    <div class="span4">
+        <br />
+        <p>
+            La Bible, version J.N. Darby.
+        </p>
+        <p>
+            <a href="<?php echo base_url(); ?>bible/search" class="bold" title="Rechercher dans la Bible J.N. Darby">Moteur de recherche</a><br />
+            <a href="<?php echo base_url(); ?>bible" class="bold" title="Lire la Bible">Lecture</a><br />
+        </p>
     </div>
-    
-    <div class="main_categories">
-        <table width="100%">
-            <tr>
-                <td>
-                    <br />
-                    <p>
-                        Recueil de cantiques pour les enfants.
-                    </p>
-                    <p>
-                        <a href="<?php echo base_url(); ?>songs/search/cantiques-pour-les-enfants" class="bold" title="Rechercher dans le recueil Cantiques pour les enfants">Moteur de recherche</a><br />
-                        <a href="<?php echo base_url(); ?>songs/index/cantiques-pour-les-enfants" class="bold" title="Index du recueil Cantiques pour les enfants">Index</a><br />
-                    </p>
-                </td>
-                <td width="200px">
-                    <a class="main_categories_logo cpe_logo" href="<?php echo base_url(); ?>songs/readall/cantiques-pour-les-enfants"></a>
-                </td>
-            </tr>
-        </table>
+</div>
+<div class="row main-category">
+    <div class="span4 offset2">
+        <a class="main_categories_logo hetc_logo" href="<?php echo base_url(); ?>songs/readall/hymnes-et-cantiques"></a>
+    </div>
+    <div class="span4">
+        <br />
+        <p>
+            Recueil de cantiques.
+        </p>
+        <p>
+            <a href="<?php echo base_url(); ?>songs/search/hymnes-et-cantiques" class="bold" title="Rechercher dans le recueil Hymnes et Cantiques">Moteur de recherche</a><br />
+            <a href="<?php echo base_url(); ?>songs/index/hymnes-et-cantiques" class="bold" title="Index du recueil Hymnes et Cantiques">Index</a><br />
+        </p>
+    </div>
+</div>
+<div class="row main-category">
+    <div class="span4 offset2">
+        <a class="main_categories_logo cpe_logo" href="<?php echo base_url(); ?>songs/readall/cantiques-pour-les-enfants"></a>
+    </div>
+    <div class="span4">
+        <br />
+        <p>
+            Recueil de cantiques pour les enfants.
+        </p>
+        <p>
+            <a href="<?php echo base_url(); ?>songs/search/cantiques-pour-les-enfants" class="bold" title="Rechercher dans le recueil Cantiques pour les enfants">Moteur de recherche</a><br />
+            <a href="<?php echo base_url(); ?>songs/index/cantiques-pour-les-enfants" class="bold" title="Index du recueil Cantiques pour les enfants">Index</a><br />
+        </p>
     </div>
 </div>
